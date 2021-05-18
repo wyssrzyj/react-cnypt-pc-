f1755769441ebeb94839460d0504efa4af5952bd
mengyajiao <myj210517@163.com>
2021-05-18 09:20:22 +0800

    feat：订单查询

--- src/pages/order/components/card/index.tsx
+++ src/pages/order/components/card/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
 const Card = () => {
-  return (
+  return (<div>
+    <img />
+    <h5>油画少女纯棉小吊带背心外搭</h5>   
+    <ul>
+      <li><span>发布时间：</span><span>2021-05-13</span></li>
+      <li><span>订单类型：</span><span>经销单，来图/来样加工</span></li>
+      <li><span>销售市场：</span><span>内销</span></li>
+      <li><span>销售件数</span><span>1500件</span></li>
+      <li><span>单价：</span><span>￥ <b>10</b></span></li>
+    </ul>
     <div>
-      sss
-      
+      <span>杭州高酷电子商务...</span>
+      <span>浙江 杭州市</span>
     </div>
-  )
+      
+  </div>)
 }
 
 export default Card
+

e93825dae3bf4be289c4166887fc13e503d9a507
mengyajiao <myj210517@163.com>
2021-05-25 16:23:21 +0800

    feat：表头

--- src/components/detailHeader/index.tsx
+++ src/components/detailHeader/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { useLocation } from 'react-router'
 import { Input } from 'antd'
-// import { AudioOutlined } from '@ant-design/icons';
 import styles from './index.module.less'
 
 const { Search } = Input
 
 const DetailHeader = () => {
+  const location = useLocation()
   const onSearch = () => {}
   return (
     <div className={styles.detailHeader}>
@@ -15,9 +16,11 @@ const DetailHeader = () => {
       />
       <Search
         className={styles.detailSearch}
-        placeholder="请输入订单信息"
+        placeholder={`请输入${
+          location.pathname === '/factory-detail' ? '工厂' : '订单'
+        }信息`}
         allowClear
-        enterButton="Search"
+        enterButton="搜索"
         size="large"
         onSearch={onSearch}
       />

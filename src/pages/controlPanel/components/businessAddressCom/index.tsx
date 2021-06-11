cc36b714d9090b8eef749908aacb5b553895518c
金阿敏 <13587760358@163.com>
2021-06-11 15:06:26 +0800

    feat(地图): 地图
    g

--- src/pages/controlPanel/components/businessAddressCom/index.tsx
+++ src/pages/controlPanel/components/businessAddressCom/index.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from 'react' // useImperativeHandle // useCallback, // useState, // useRef, // useEffect,
 import { Input } from 'antd'
-import { Scene, Marker } from '@antv/l7'
+import { Scene, Marker, MarkerLayer, PointLayer } from '@antv/l7'
 import { GaodeMap } from '@antv/l7-maps'
 import styles from './index.module.less'
 
@@ -10,28 +10,34 @@ const BusinessAddressCom = () => {
   const markerRef = useRef()
 
   useEffect(() => {
-    const scene = new Scene({
-      id: 'yjMap',
-      map: new GaodeMap({
-        style: 'light',
-        center: [120.19382669582967, 30.258134],
-        pitch: 0,
-        zoom: 10,
-        rotation: 0,
-        viewMode: '2D'
-      })
+    const mapBox: any = new GaodeMap({
+      style: 'light',
+      center: [105.790327, 36.495636],
+      pitch: 0,
+      zoom: 4
+    })
+
+    const scene: any = new Scene({
+      id: `yjMap`,
+      logoVisible: false,
+      map: mapBox
     })
 
     console.log(scene, 'scene')
 
-    const marker = new Marker({
-      element: markerRef.current
-    })
-    // { lng: 120.2, lat: 30.3 }
-    marker.setLnglat({ x: 120.2, y: 30.3 })
-    scene.addMarker(marker)
-
     scene.on('loaded', () => {
+      const markerLayer = new MarkerLayer()
+      const el = document.createElement('label')
+      el.className = 'labelclass'
+      el.style.background =
+        'url(https://gw.alipayobjects.com/mdn/antv_site/afts/img/A*BJ6cTpDcuLcAAAAAAAAAAABkARQnAQ)'
+      const marker = new Marker({
+        element: el
+      }).setLnglat({ lng: 120.2, lat: 30.3 })
+      markerLayer.addMarker(marker)
+
+      scene.addMarkerLayer(markerLayer)
+
       console.log('~~~~~')
     })
   }, [])

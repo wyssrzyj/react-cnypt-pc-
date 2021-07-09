26d442d25aca54beb7cd85b98207c5b033a0fd56
mengyajiao <myj210517@163.com>
2021-07-09 11:03:33 +0800

    feat：导航栏调整

--- src/layout/newHeader.tsx
+++ src/layout/newHeader.tsx
@@ -2,14 +2,61 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useHistory } from 'react-router'
 import { Menu, Dropdown } from 'antd'
+import { isEmpty } from 'lodash'
 import { getCurrentUser } from '@/utils/tool'
 import styles from './newHeader.module.less'
 import { useStores } from '@/utils/mobx'
+
+const consoleOptions = [
+  {
+    title: '企业管理',
+    children: [
+      {
+        title: '企业信息',
+        url: '/control-panel/enterprise'
+      },
+      {
+        title: '企业证件认证',
+        url: '/control-panel/certificate'
+      }
+    ]
+  },
+  {
+    title: '资质认证',
+    children: [
+      {
+        title: '资质认证',
+        url: '/control-panel/qualification'
+      }
+    ]
+  },
+  {
+    title: '验厂管理',
+    children: [
+      {
+        title: '基础资料报告',
+        url: '/control-panel/report'
+      },
+      {
+        title: '车间设备',
+        url: '/control-panel/equipment'
+      },
+      {
+        title: '工厂照片',
+        url: '/control-panel/photograph'
+      }
+    ]
+  }
+]
 
 const Header = () => {
   const currentUser = getCurrentUser() || {}
   const { loginStore } = useStores()
   const { logout } = loginStore
+  const { approvalStatus, factoryAuditStatus } = currentUser
+  const newConsoleOptions = consoleOptions
+    .filter(item => !(approvalStatus && item.title === '资质认证'))
+    .filter(obj => !(factoryAuditStatus == '1' && obj.title === '验厂管理'))
 
   const history = useHistory()
 
@@ -29,20 +76,47 @@ const Header = () => {
   const menu = (
     <Menu>
       <Menu.Item>
-        <Link to="/control-panel/enterprise">控制台</Link>
+        <div>账号安全</div>
       </Menu.Item>
       <Menu.Item>
         <div onClick={logoutToLogin}>退出登录</div>
       </Menu.Item>
     </Menu>
   )
+  const consoleMenu = (
+    <div className={styles.console}>
+      {newConsoleOptions.map((item, index) => {
+        const { title, children } = item
+        return (
+          <div key={index}>
+            <div className={styles.title}>{title}</div>
+            <div className={styles.titleContent}>
+              {!isEmpty(children) &&
+                children.map((o, index) => {
+                  return (
+                    <Link key={index} to={o.url} className={styles.routerItem}>
+                      {o.title}
+                    </Link>
+                  )
+                })}
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
 
   return (
     <header className={styles.header}>
       <div>
-        欢迎来到Jack产能云平台，
-        {currentUser.nickName ? (
-          <span>{currentUser.nickName}</span>
+        <Link to="/home" className={styles.home}>
+          产能云平台首页
+        </Link>
+
+        {currentUser.realName ? (
+          <Dropdown overlay={menu}>
+            <span className={styles.user}>您好，{currentUser.realName}</span>
+          </Dropdown>
         ) : (
           <>
             <span>请</span>
@@ -58,9 +132,8 @@ const Header = () => {
       </div>
 
       <div className={styles.chunks}>
-        <span className={styles.headerChunk}>帮助中心</span>
-        <Dropdown overlay={menu}>
-          <span className={styles.headerChunk}>会员中心</span>
+        <Dropdown overlay={consoleMenu} trigger={['click']}>
+          <span className={styles.headerChunk}>控制台</span>
         </Dropdown>
       </div>
     </header>

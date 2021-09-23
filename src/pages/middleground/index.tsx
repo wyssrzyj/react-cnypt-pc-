94620d7a50e22acb3eed61d972859e479fec382c
luyingjie <lyj1974451304@163.com>
2021-09-23 09:35:49 +0800

    feat(视频列表): 视频列表

--- src/pages/middleground/index.tsx
+++ src/pages/middleground/index.tsx
@@ -7,30 +7,20 @@ import { getUserInfo } from '@/utils/tool'
 
 const Factory = React.lazy(() => import('./enterpriseHome/factory'))
 const Business = React.lazy(() => import('./enterpriseHome/business'))
-const Issueabill = React.lazy(() => import('./Issueabilltwo'))
 const MonitorPage = React.lazy(() => import('./monitorPage'))
-const NewlyAdded = React.lazy(
-  () => import('./Issueabilltwo/components/newlyAdded')
-)
 
 const LOGO =
   'http://capacity-platform.oss-cn-hangzhou.aliyuncs.com/capacity-platform/20210722/5a113adbb7a24ecc8ebedef760019f84.png'
 
 const paths = ['/control-panel/home']
-const pathsIssuable = ['/control-panel/Issueabill']
-const pathsAccount = ['/control-panel/panel/account']
 const routeMap = new Map()
 routeMap.set(0, Factory)
 routeMap.set(1, Business)
-
-const ControlPanel = React.lazy(() => import('./controlPanel'))
 
 const EnterpriseHome = () => {
   const location = useLocation()
   const history = useHistory()
   const flag = paths.includes(location.pathname)
-  const flagIssuable = pathsIssuable.includes(location.pathname)
-  const flagAccount = pathsAccount.includes(location.pathname)
   const [userInfo, setUserInfo] = useState({ enterpriseType: null })
 
   useEffect(() => {
@@ -43,7 +33,6 @@ const EnterpriseHome = () => {
   const toTarget = type => {
     type === 'control' && history.push('/control-panel/panel/account')
     type !== 'control' && history.push('/control-panel/home')
-    type === 'Issueabill' && history.push('/control-panel/Issueabill')
   }
 
   return (
@@ -63,42 +52,12 @@ const EnterpriseHome = () => {
                 首页
               </div>
             ) : null}
-
-            <div
-              className={classNames(
-                styles.navItem,
-                flagIssuable ? styles.activeNavItem : ''
-              )}
-              onClick={() => toTarget('Issueabill')}
-            >
-              发单管理
-            </div>
-            <div
-              className={classNames(
-                styles.navItem,
-                flagAccount ? styles.activeNavItem : ''
-              )}
-              onClick={() => toTarget('control')}
-            >
-              会员中心
-            </div>
           </div>
         </div>
       </div>
 
       <div className={styles.content}>
         <Switch>
-          <Route path="/control-panel/Issueabill" component={Issueabill} />
-          <Route
-            path="/control-panel/components/newlyAdded"
-            component={NewlyAdded}
-          />
-
-          <Route path="/control-panel/panel" component={ControlPanel} />
-          <Route
-            path="/control-panel/home"
-            component={routeMap.get(+userInfo.enterpriseType)}
-          />
           <Route path="/control-panel/monitorPage" component={MonitorPage} />
         </Switch>
       </div>

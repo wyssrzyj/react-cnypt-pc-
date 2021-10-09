15e8093ca672e973d0c0c5f28573d2a68fee18f8
金阿敏 <13587760358@163.com>
2021-10-09 08:34:56 +0800

    feat(修改): 修改

---
+++ src/pages/middleground/controlPanel/routeConfig.tsx
@@ -0,0 +1,36 @@
+export const menuKeys = new Map()
+menuKeys.set('/control-panel/panel/qualification', [
+  'qualification',
+  'sub2',
+  'sub1'
+])
+// 对应修改菜单的key值
+menuKeys.set('/control-panel/panel/account', ['account', 'sub1'])
+menuKeys.set('/control-panel/panel/enterprise', ['enterprise', 'sub1'])
+menuKeys.set('/control-panel/panel/issue-bill', ['issue-bill', 'sub1'])
+menuKeys.set('/control-panel/panel/logs', ['account', 'sub1'])
+menuKeys.set('/control-panel/panel/qualification', ['qualification', 'sub2'])
+menuKeys.set('/control-panel/panel/certificate', ['certificate', 'sub2'])
+menuKeys.set('/control-panel/panel/photo', ['photo', 'sub3'])
+menuKeys.set('/control-panel/panel/report', ['report', 'sub3'])
+menuKeys.set('/control-panel/panel/equipment', ['equipment', 'sub3'])
+menuKeys.set('/control-panel/panel/photograph', ['photograph', 'sub3'])
+menuKeys.set('/control-panel/panel/monitorPage', ['monitorPage', 'sub4'])
+menuKeys.set('/control-panel/panel/video-center', ['videoCenter', 'sub4'])
+menuKeys.set('/control-panel/panel/department', ['department', 'sub4'])
+menuKeys.set('/control-panel/panel/information', ['information', 'sub1'])
+
+export const subsMap = new Map()
+subsMap.set('/control-panel/panel/account', ['sub1'])
+subsMap.set('/control-panel/panel/enterprise', ['sub1'])
+subsMap.set('/control-panel/panel/issue-bill', ['sub1'])
+subsMap.set('/control-panel/panel/logs', ['sub1'])
+subsMap.set('/control-panel/panel/qualification', ['sub2'])
+subsMap.set('/control-panel/panel/certificate', ['sub2'])
+subsMap.set('/control-panel/panel/photo', ['sub3'])
+subsMap.set('/control-panel/panel/report', ['sub3'])
+subsMap.set('/control-panel/panel/equipment', ['sub3'])
+subsMap.set('/control-panel/panel/photograph', ['sub3'])
+subsMap.set('/control-panel/panel/video-center', ['sub4'])
+subsMap.set('/control-panel/panel/monitorPage', ['sub4'])
+subsMap.set('/control-panel/panel/department', ['sub4'])

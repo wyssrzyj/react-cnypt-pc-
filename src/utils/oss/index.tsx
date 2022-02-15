04b6417e26505dee377b16289e1742cc59501562
luyingjie <lyj1974451304@163.com>
2022-02-15 15:54:38 +0800

    feat(t): t

--- src/utils/oss/index.tsx
+++ src/utils/oss/index.tsx
@@ -3,10 +3,10 @@ import * as OSS from 'ali-oss'
 const client = new OSS({
   accessKeyId: process.env.ACCESS_KEY_ID,
   accessKeySecret: process.env.ACCESS_KEY_SECRET,
-  regoin: 'oss-cn-hangzhou',
+  region: 'oss-cn-hangzhou',
   endpoint: 'oss-cn-hangzhou.aliyuncs.com',
   bucket: 'capacity-platform',
   timeout: 600000
-})
+} as any)
 
 export default client
